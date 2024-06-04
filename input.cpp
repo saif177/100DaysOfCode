eb10125ee3bf8f451c771e6af07cbaf8b911e455
Saif Ali <saif.blt17@gmail.com>
2024-06-05 00:05:33 +0530

    simple-cpp input

---
+++ input.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main() {
+    string uName;
+    cout << "enter your name" <<endl;
+    cin>>uName;
+
+    cout << "Your Name Is " << uName << endl;
+
+    return 0;
+}
